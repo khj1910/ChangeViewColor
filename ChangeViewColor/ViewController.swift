7078082e8278b669ccccd95618c379a4cc7ffce4
D7703_23 <d7703_23@d7703-23ui-imac.local>
2019-03-19 15:36:12 +0900

    Toggle

--- ChangeViewColor/ViewController.swift
+++ ChangeViewColor/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ColorState = true
+    @IBOutlet weak var lblL: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btnP(_ sender: Any) {
+        lblL.text = "Hello Button Pressed!"
+    }
+    
+    @IBAction func btnI(_ sender: Any) {
+        lblL.text = ""
+    }
+    
+    @IBAction func btnC(_ sender: Any) {
+        //toggle 스위치
+        if ColorState == true{
+            self.view.backgroundColor = UIColor.yellow
+            ColorState = false
+        }else{
+        self.view.backgroundColor = UIColor.white
+            ColorState = true
+    }
+    
 }
 
+}
